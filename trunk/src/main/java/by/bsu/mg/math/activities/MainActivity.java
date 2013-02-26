22619364693eff695979ba8eb23e4bcf2ba75dd1
vmgoshko@gmail.com <vmgoshko@gmail.com@31d5c7af-6187-6637-f47f-300e3e7b04d1>
2013-02-26 14:30:30 +0000

    Page flipping added; KeysPagerAdapter.java added;

--- trunk/src/main/java/by/bsu/mg/math/activities/MainActivity.java
+++ trunk/src/main/java/by/bsu/mg/math/activities/MainActivity.java
@@ -2,15 +2,14 @@ package by.bsu.mg.math.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
 import by.bsu.mg.math.R;
-import by.bsu.mg.math.computing.Calculator;
-import by.bsu.mg.math.computing.Differentiator;
-import by.bsu.mg.math.parsing.expressions.ExpressionBuilder;
-import by.bsu.mg.math.parsing.expressions.ExpressionOptimizer;
-import by.bsu.mg.math.parsing.expressions.Node;
-import by.bsu.mg.math.utils.IPoint;
 import by.bsu.mg.math.views.GLGraphPlane;
+import by.bsu.mg.math.views.KeysPagerAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends Activity {
@@ -20,19 +19,18 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-        ExpressionBuilder exprBuilder = new ExpressionBuilder();
-        ExpressionOptimizer optimizer = new ExpressionOptimizer();
-
-        Node expr = exprBuilder.buildTree("sin(x)");
-        optimizer.optimize(expr);
-
-        Calculator calc = new Calculator();
-        List<IPoint> points = calc.calculate(expr, -10, 10);
-
-        // setContentView(new GLGraphPlane(this,points));
         setContentView(R.layout.calculator);
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        List<View> pages = new ArrayList<View>();
+
+        pages.add(inflater.inflate(R.layout.first, null));
+        pages.add(inflater.inflate(R.layout.second, null));
+
+        KeysPagerAdapter keysPagerAdapter = new KeysPagerAdapter(pages);
+        ViewPager viewPager = (ViewPager)findViewById(R.id.pages);
+        viewPager.setAdapter(keysPagerAdapter);
+        viewPager.setCurrentItem(1);
     }
 
     @Override
