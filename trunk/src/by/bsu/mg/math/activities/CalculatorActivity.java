81f21dbdf7c198353cf91bc9d1b4d4833d08d43a
vmgoshko@gmail.com <vmgoshko@gmail.com@31d5c7af-6187-6637-f47f-300e3e7b04d1>
2013-04-03 06:52:10 +0000

    - Added class timer for debugging; - Added all needed classes for application working;

---
+++ trunk/src/by/bsu/mg/math/activities/CalculatorActivity.java
@@ -0,0 +1,53 @@
+package by.bsu.mg.math.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import by.bsu.mg.math.views.GLGraphPlane;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CalculatorActivity extends Activity {
+
+    private GLGraphPlane graphPlane;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculator);
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        List<View> pages = new ArrayList<View>();
+
+        pages.add(inflater.inflate(R.layout.func_keyb, null));
+        pages.add(inflater.inflate(R.layout.num_keyb, null));
+        pages.add(inflater.inflate(R.layout.const_keyb, null));
+
+        KeysPagerAdapter keysPagerAdapter = new KeysPagerAdapter(pages);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pages);
+        viewPager.setAdapter(keysPagerAdapter);
+        viewPager.setCurrentItem(1);
+
+        ExpressionEditText et = (ExpressionEditText) findViewById(R.id.editText);
+        TextView outView = (TextView)findViewById(R.id.textView);
+        KeyboardClickListener keyboardClickListener = new KeyboardClickListener(this, et, viewPager, outView);
+        keyboardClickListener.registerListener();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //  graphPlane.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //  graphPlane.onResume();
+    }
+}
